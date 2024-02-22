eda75b2c6d4305a90901e7cc14a70c16ffb1f9eb
keerthu1910 <keerthiselva1910@gmail.com>
2024-02-22 18:13:53 +0530

    Include path for features component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,1 +1,2 @@
 export * from './Landingscreen';
+export * from './Features';
--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Landingscreen } from '../Components';
+import { Features } from '../Components';
 export const Landingpage = () => {
     return(
         <>
             <Landingscreen />
+            <Features />
         </>
     )
 }
