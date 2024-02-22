aea4f1e7cabc54d6cff60672c14044cd774875d7
keerthu1910 <keerthiselva1910@gmail.com>
2024-02-22 17:24:16 +0530

    Create landingscreen component

---
+++ src/Components/Landingscreen/Landingscreen.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.scss';
+import logo from '../../assets/logo.png';
+import logotext from '../../assets/ANALYTICS.png';
+import textblockimage from '../../assets/textblockimage.png';
+export const Landingscreen = () => {
+    return(
+        <div className='container' >
+            <div className='navbar-container'>
+                <div className='logo-container'>
+                    <img src={logo} alt='logo'/><span><img src={logotext} alt='analytics'/></span>
+                </div>
+                <div className='menu-container'>
+                    <a>Products</a>
+                    <a>Pricing</a>
+                    <a>FAQ</a>
+                    <a>Blog</a>
+                    <a>Services</a>
+                </div>
+                <div className='btn-container'>
+                    <button>Sign in</button>
+                    <button className='sign-up-btn'>Sign Up</button>
+                </div>
+            </div>
+            <div className='textblock-container'>
+                <div className='text-block'>
+                    <p className='heading-text'>Monitor your business <br></br> on real-time dashboard</p>
+                    <p className='block-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
+                    <button>Try Now</button>
+                </div>
+                <div className='image-block'>
+                    <img src={textblockimage} alt='analyticsimage'/>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
