aeb8a7d7d988457366dca9f68da6b642c0885055
keerthu1910 <keerthiselva1910@gmail.com>
2024-02-22 17:24:45 +0530

    Add path

---
+++ src/Pages/Landingpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Landingscreen } from '../Components';
+export const Landingpage = () => {
+    return(
+        <>
+            <Landingscreen />
+        </>
+    )
+}
