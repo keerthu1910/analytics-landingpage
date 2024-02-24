b2f5775860a64d9d1de566e6dcc2b5e075fca4dc
keerthu1910 <keerthiselva1910@gmail.com>
2024-02-24 22:44:25 +0530

    Include path for the priceplan and footer component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,5 @@
 export * from './Landingscreen';
 export * from './Features';
 export * from './Reports';
+export * from './Pricingplans';
+export * from './Footer';
--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Landingscreen } from '../Components';
 import { Features } from '../Components';
 import { Reports } from '../Components';
+import { Pricingplan } from '../Components';
+import { Footer } from '../Components';
 export const Landingpage = () => {
     return(
         <>
             <Landingscreen />
             <Features />
             <Reports />
+            <Pricingplan />
+            <Footer />
         </>
     )
 }
