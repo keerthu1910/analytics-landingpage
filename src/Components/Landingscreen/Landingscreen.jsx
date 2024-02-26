2584d4244a91bb0a23ff7fb2e82f2ba2a4016cc4
keerthu1910 <keerthiselva1910@gmail.com>
2024-02-26 15:28:31 +0530

    Include changes to have responsive menu bar

--- src/Components/Landingscreen/Landingscreen.jsx
+++ src/Components/Landingscreen/Landingscreen.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.scss';
 import logo from '../../assets/logo.png';
 import logotext from '../../assets/ANALYTICS.png';
 import textblockimage from '../../assets/textblockimage.png';
 export const Landingscreen = () => {
+    const [hidemenu,setHidemenu] = useState(false);
+    const handleMenu = () => {
+        setHidemenu(!hidemenu)
+    }
     return(
         <div className='container' >
             <div className='navbar-container'>
@@ -18,15 +22,30 @@ export const Landingscreen = () => {
                     <a>Services</a>
                 </div>
                 <div className='btn-container'>
-                    <button>Sign in</button>
+                    <button className='sign-in-btn'>Sign in</button>
                     <button className='sign-up-btn'>Sign Up</button>
                 </div>
+                <div className='hamburger-menu' >
+                    <button onClick={()=>setHidemenu(!hidemenu)}>{'\u2630'}</button>
+                </div>
+            </div>
+            <div className={hidemenu?'normal':'vertical-menu-container'}>
+                    <div className='first-menu'>
+                    <a>Products</a>
+                    <button onClick={()=>setHidemenu(!hidemenu)}>{'\u2715'}</button>
+                    </div>
+                    <a>Pricing</a>
+                    <a>FAQ</a>
+                    <a>Blog</a>
+                    <a>Services</a>
+                    <button className='sign-in-btn'>Sign in</button>
+                    <button className='sign-up-btn'>Sign Up</button>
             </div>
             <div className='textblock-container'>
                 <div className='text-block'>
                     <p className='heading-text'>Monitor your business <br></br> on real-time dashboard</p>
                     <p className='block-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. </p>
-                    <button>Try Now</button>
+                    <button className='text-btn'>Try Now</button>
                 </div>
                 <div className='image-block'>
                     <img src={textblockimage} alt='analyticsimage'/>
